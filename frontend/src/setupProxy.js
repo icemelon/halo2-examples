adda05985d716b21420f79c90c0f32f932c5b8f3
Uma Roy <uma.roy.us@gmail.com>
2022-07-06 23:44:28 +0000

    Added CORS shit to react

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,9 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+        next();
+    });
+};
